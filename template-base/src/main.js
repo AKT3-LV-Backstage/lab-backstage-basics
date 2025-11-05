92569076ce99bbd6e97660e19ee9ed8bf5eee92e
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2025-11-05 17:22:50 +0100

    chore: add some files
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

---
+++ template-base/src/main.js
@@ -0,0 +1,15 @@
+// A simple Node.js server that responds with a greeting message
+// After setting up a new project, this should respond with a message containing the templated project name
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('Hello from the template-base project!\n');
+});
+
+const port = 8000;
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
